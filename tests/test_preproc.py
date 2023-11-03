8d12d0c7d08ce4e3f8f940976e98dd1d6728ec42
Andres M Bran <doncamilom@gmail.com>
2023-11-03 19:52:00 +0100

    skip old test

--- tests/test_preproc.py
+++ tests/test_preproc.py
@@ -5,7 +5,7 @@ import pytest
 from choriso.data.processing.preproc import *
 
 
-#skip this test
+# skip this test
 @pytest.mark.skip(reason="not implemented yet")
 def test_get_structures_from_name():
     """Test get_structures_from_name, text to SMILES function.
